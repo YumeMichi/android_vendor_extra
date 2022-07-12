4986c2bc03ed7357665cdf515ddfef2417a38dc3
Yuan Si <do4suki@gmail.com>
2022-07-12 22:51:41 +0800

    Build ih8sn
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: Icc0f7b2fcb3e32ab93b1d2f4a77858de51769d85

--- product.mk
+++ product.mk
@@ -7,6 +7,12 @@ ifeq ($(WITH_GMS), true)
 $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 endif
 
+# ih8sn
+PRODUCT_PACKAGES += ih8sn
+
+PRODUCT_COPY_FILES += \
+    vendor/extra/ih8sn/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
+
 # iperf3
 PRODUCT_PACKAGES += iperf3
 
